81a7569bc7c84726131b31edc5fd7b4b1bddec1c
Kauã Maximino <kaua_maximino@outlook.com>
2022-08-15 18:34:14 -0300

    feat: criando objetos das classes criadas anteriormente

--- src/Character.ts
+++ src/Character.ts
@@ -20,7 +20,7 @@ class Character implements Fighter {
     this._dexterity = getRandomInt(1, 10);
     this._race = new Elf(name, this._dexterity);
     this._archetype = new Mage(name);
-    this._maxLifePoints = Math.round(this._race.maxLifePoints / 2);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
     this._strength = getRandomInt(1, 10);
     this._defense = getRandomInt(1, 10);
